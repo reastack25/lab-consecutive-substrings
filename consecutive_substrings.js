624840f17f99bca9e5e0b00c44f3edf2e749cd27
reagan <221689087+reastack25@users.noreplygithub.com>
2025-12-25 10:00:30 +0300

    addition of my own test and algorithm

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,22 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+  
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      result.push(string.slice(i, j + 1));
+    }
+  }
+  
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log(consecutiveSubstrings(""))
+  console.log(consecutiveSubstrings("a")) 
+  console.log(consecutiveSubstrings("ab")) 
+  console.log(consecutiveSubstrings("abc")) 
+  console.log(consecutiveSubstrings("hello")) 
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
